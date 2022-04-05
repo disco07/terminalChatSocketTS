a9519108ba7700cbc982836ca8e557a55a2db790
disco07 <koneenok@outlook.fr>
2022-04-05 13:04:22 +0200

    chat socketio typscript finish

--- chat-client.ts
+++ chat-client.ts
@@ -1,15 +1,23 @@
 const socket = require('socket.io-client')('http://localhost:3000'); //communique sur l'adresse qui dans le server socket
 const repl = require('repl'); // permet de créer un terminal
 
+// ('disconnect') evenement natif de socket io qui permet de détecter si un user est deconnecté
 socket.on('disconnect', () => {
+    // socket.emit permet d'envoyer un donnée, alors que socket.on permet de recevoir
     socket.emit('disconnect')
 });
+
+// ('connect') evenement natif de socket io qui permet de détecter si un user est connecté
 socket.on('connect', () => {
     console.log('=== demarrage du chat ===')
 })
+
+// ('message') evenement que j'ai crée, il permet d'envoyé des messages
 socket.on('message', (data) => {
     console.log(data);
 })
+
+// repl permet de demarrer le chat dans un terminal
 repl.start({
     prompt: '',
     eval: (cmd) => {
--- chat-node.ts
+++ chat-node.ts
@@ -1,10 +1,13 @@
 const http = require('http').createServer();
 const io = require('socket.io')(http);
 const PORT = 3000
+
 io.on('connection', (socket) => {
     console.log('connected')
     socket.on('message', (evt) => {
         console.log(evt)
+        // les informations envoyées depuis le clients sont recus par le server et dispatchés par ici
+        // socket.broadcast.emit permet d'envoyer à toutes les personnes connectées sauf à soi-meme
         socket.broadcast.emit('message', evt)
     })
 })
