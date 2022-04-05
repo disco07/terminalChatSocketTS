2ad3f374c8e43c1708ebed3886ec706baddac8e3
disco07 <koneenok@outlook.fr>
2022-04-05 12:36:24 +0200

    chat demarrage

--- chat-client.ts
+++ chat-client.ts
@@ -1,15 +1,15 @@
-const socket = require('socket.io-client')('http://localhost:3000');
-const repl = require('repl')
-const chalk = require('chalk');
+const socket = require('socket.io-client')('http://localhost:3000'); //communique sur l'adresse qui dans le server socket
+const repl = require('repl'); // permet de créer un terminal
+
 socket.on('disconnect', function() {
     socket.emit('disconnect')
 });
 socket.on('connect', () => {
-    console.log(chalk.red('=== start chatting ==='))
+    console.log('=== demarrage du chat ===')
 })
-socket.on('message', (data: { cmd: any; username: any; }) => {
+socket.on('message', (data) => {
     const { cmd, username } = data
-    console.log(chalk.green(username + ': ' + cmd.split('\n')[0]));
+    console.log(username + ': ' + cmd.split('\n')[0]);
 })
 repl.start({
     prompt: '',
